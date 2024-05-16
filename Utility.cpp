eecd0cbce79749c664654653b6a03125f87ef42b
Shasin <166836376+ShasinRegmi@users.noreply.github.com>
2024-05-16 19:48:08 +0545

    Updated Utility.cpp

--- Utility.cpp
+++ Utility.cpp
@@ -14,12 +14,10 @@ namespace Utility {
             value.push_back('0');
             Inorder(root->left, value);
             value.pop_back();
-
             if (root->left == nullptr && root->right == nullptr) {
                 cout << "Character: " << root->character << ", Count: " << root->count << ", ";
                  cout << "Huffman Value: " << value <<  endl;
             }
-            
             value.push_back('1');
             Inorder(root->right, value);
             value.pop_back();
